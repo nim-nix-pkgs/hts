fcf69dba3e8a6b4af39f346ae85b4855086a0bd9
hugosenari <hugosenari@gmail.com>
2022-09-03 19:48:25 -0300

    feat(nimpkgs): update lock

--- source/src/hts/utils.nim
+++
@@ -1,3 +0,0 @@
-import ./private/hts_concat
-proc free*(a1: pointer) {.cdecl, importc: "free", header: "<stdlib.h>".}
-export kstring_t, free, hts_open, hts_close
--- source/tests/all.nim
+++
@@ -1,2 +0,0 @@
-{. warning[UnusedImport]:off .}
-import flagtest, cigartest, htstest, bgzftest, faitest, auxtest, vcftest, bamtest, statstests, vcfiso, test_files, test_readme
